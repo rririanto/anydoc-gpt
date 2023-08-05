83b464cd72a9e495df04caecd30890e1412a4c5f
Rahmat Ramadhan Irianto <rahmat.ramadhaniriyanto@gmail.com>
2023-08-05 10:05:20 +0700

    fix: memory issues

--- cores/openai_models.py
+++ cores/openai_models.py
@@ -2,8 +2,9 @@ import requests
 import streamlit as st
 
 
-@st.cache_data()
 def get_gpt_models(api_key):
+    '''
+    Uncomment this if you want to get realtime models
     headers = {
         'authority': 'api.openai.com',
         'accept': 'application/json, text/plain, */*',
@@ -27,3 +28,5 @@ def get_gpt_models(api_key):
     print([model['id'] for model in rsp['data']
           if 'gpt' or 'davinci' in model['id'].lower()])
     return [model['id'] for model in rsp['data'] if 'gpt' or 'davinci' in model['id'].lower()]
+    '''
+    return ['text-davinci-001', 'text-search-curie-query-001', 'davinci', 'text-babbage-001', 'curie-instruct-beta', 'text-davinci-003', 'davinci-similarity', 'code-davinci-edit-001', 'text-similarity-curie-001', 'text-embedding-ada-002', 'ada-code-search-text', 'text-search-ada-query-001', 'babbage-search-query', 'ada-similarity', 'gpt-3.5-turbo', 'whisper-1', 'text-search-ada-doc-001', 'text-search-babbage-query-001', 'code-search-ada-code-001', 'curie-search-document', 'text-search-davinci-query-001', 'text-search-curie-doc-001', 'gpt-3.5-turbo-0301', 'babbage-search-document', 'babbage-code-search-text', 'davinci-instruct-beta', 'davinci-search-query', 'text-similarity-babbage-001', 'text-davinci-002', 'code-search-babbage-text-001', 'babbage', 'text-search-davinci-doc-001', 'code-search-ada-text-001', 'gpt-3.5-turbo-16k-0613', 'gpt-3.5-turbo-16k', 'ada-search-query', 'text-similarity-ada-001', 'ada-code-search-code', 'ada', 'text-davinci-edit-001', 'davinci-search-document', 'curie-search-query', 'babbage-similarity', 'ada-search-document', 'text-ada-001', 'text-similarity-davinci-001', 'curie', 'curie-similarity', 'gpt-3.5-turbo-0613', 'babbage-code-search-code', 'code-search-babbage-code-001', 'text-search-babbage-doc-001', 'text-curie-001']
